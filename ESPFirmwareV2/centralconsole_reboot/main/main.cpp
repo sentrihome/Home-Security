28a1b39dc496215c6f4718741cb6b989a12e34ef
Anurag Biswas Koushik <anurag.koushik69@gmail.com>
2026-06-11 22:48:03 -0600

    Display Configuration

--- ESPFirmwareV2/centralconsole_reboot/main/main.cpp
+++ ESPFirmwareV2/centralconsole_reboot/main/main.cpp
@@ -10,5 +10,5 @@ void setup(){
 }
 
 void loop(){
-  delay(1);
+  delay(10);
 }
---
+++ ESPFirmwareV2/centralconsole_reboot/main/uielements.h
@@ -0,0 +1,9 @@
+#ifndef UIELEMENTS_H
+#define UIELEMENTS_H
+
+#include <stdint.h>
+
+extern const uint8_t uidashboard[];
+extern const uint8_t uiarmed[];
+
+#endif 
