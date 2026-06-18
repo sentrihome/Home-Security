f8817694e13cd0d77f0d613b2928a884d72e7ea6
Anurag Biswas Koushik <anurag.koushik69@gmail.com>
2026-06-18 00:13:49 -0600

    Codebase transition to ESPIDF

--- ESPFirmwareV2/centralconsole_reboot/main/main.cpp
+++ ESPFirmwareV2/centralconsole_reboot/main/main.cpp
@@ -1,15 +1,29 @@
 #include "display.h"
-
+#include "connectivity.h"
 
 Display display;
 
-void setup(){
+// void setup(){
+//   Serial.begin(115200);
+//   display.init();
+//   delay(10);
+//   wifi_start();
+// }
+
+// void loop(){
+//   display.process();
+//   delay(200);
+// }
+
+extern "C" void app_main(void) {
+  initArduino();
   Serial.begin(115200);
   display.init();
   delay(10);
-}
+  wifi_start();
 
-void loop(){
-  display.process();
-  delay(200);
+  while(true){
+    display.process();
+    delay(200);
+  }
 }
