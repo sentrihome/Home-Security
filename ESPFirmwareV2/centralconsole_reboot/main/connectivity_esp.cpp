1895eb6490fbeec5fa7387f9dd01ebf63277798c
Anurag Biswas Koushik <anurag.koushik69@gmail.com>
2026-07-08 04:47:07 -0600

    Fixed accesspoint discovery and channel issues

--- ESPFirmwareV2/centralconsole_reboot/main/connectivity_esp.cpp
+++ ESPFirmwareV2/centralconsole_reboot/main/connectivity_esp.cpp
@@ -46,6 +46,7 @@ void wifi_init(){
     esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
 
     esp_netif_create_default_wifi_sta();
+    esp_netif_create_default_wifi_ap();
 
     wifi_init_config_t wifi_driver_config = WIFI_INIT_CONFIG_DEFAULT();
     esp_wifi_init(&wifi_driver_config);
@@ -57,8 +58,11 @@ void wifi_init(){
     wifi_config_t accesspoint_configuration = {};
     const char* ap_ssid = "espwifi";
     const char* ap_password = "23012003";
-    memcpy(accesspoint_configuration.ap.ssid, ap_ssid, sizeof(ap_ssid));
-    memcpy(accesspoint_configuration.ap.password, ap_password, sizeof(ap_password));
+    memcpy(accesspoint_configuration.ap.ssid, ap_ssid, strlen(ap_ssid) + 1);
+    memcpy(accesspoint_configuration.ap.password, ap_password, strlen(ap_password) + 1);
+    accesspoint_configuration.ap.authmode = WIFI_AUTH_WPA2_PSK;
+    accesspoint_configuration.ap.max_connection = 5;
+    accesspoint_configuration.ap.channel = 11;
 
     esp_wifi_set_config(WIFI_IF_STA, &station_configuration);
     esp_wifi_set_config(WIFI_IF_AP, &accesspoint_configuration);
